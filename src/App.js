2db468a21904f306dd6d8f8b7392e12ebc20faeb
Furkan Yıldırım <furkanyildirim3455.3455@gmail.com>
2024-03-06 01:49:45 +0100

    unvalid city info added

--- src/App.js
+++ src/App.js
@@ -1,42 +1,55 @@
 import React, { useState } from 'react';
-
 import { fetchWeather } from './api/fetchWeather';
 import './App.css';
 
 const App = () => {
     const [query, setQuery] = useState('');
     const [weather, setWeather] = useState({});
+    const [error, setError] = useState('');
 
     const search = async (e) => {
-        if(e.key === 'Enter') {
-            const data = await fetchWeather(query);
-
-            setWeather(data);
-            setQuery('');
+        if (e.key === 'Enter') {
+            try {
+                const data = await fetchWeather(query);
+                setWeather(data);
+                setQuery('');
+                setError('');
+            } catch (error) {
+                setError('City not found. Please enter a valid city name.');
+                setWeather({});
+            }
         }
-    }
+    };
+
 
     return (
         <div className="main-container">
-            <input type="text" className="search" placeholder="Search..." value={query} onChange={(e) => setQuery(e.target.value)} onKeyPress={search}/>
-            {weather.main && (
-                <div className="city">
-                    <h2 className="city-name">
-                        <span>{weather.name}</span>
-                        <sup>{weather.sys.country}</sup>
-                    </h2>
-                    <div className="city-temp">
-                        {Math.round(weather.main.temp)}
-                        <sup>&deg;C</sup>
-                    </div>
-                    <div className="info">
-                        <img className="city-icon" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
-                        <p>{weather.weather[0].description}</p>
-                    </div>
-                </div>
-            )}
+            <input type="text" className="search" placeholder="Search..." value={query} onChange={(e) => setQuery(e.target.value)} onKeyPress={search} />
+            <div className="card">
+                {weather.main && (
+                    <>
+                        <div className="card-content">
+                            <h2 className="city-name">
+                                <span>{weather.name}</span>
+                                <sup>{weather.sys.country}</sup>
+                            </h2>
+                            <div className="city-temp">
+                                {Math.round(weather.main.temp)}
+                                <sup>&deg;C</sup>
+                            </div>
+                            <div className="info">
+                                <img className="city-icon" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description}/>
+                                <p>{weather.weather[0].description}</p>
+                            </div>
+                        </div>
+                    </>
+                )}
+                {error && (
+                    <div className="error">{error}</div>
+                )}
+            </div>
         </div>
     );
-}
+};
 
 export default App;
